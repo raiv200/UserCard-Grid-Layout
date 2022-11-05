4639af256846ef26c80667977e4634d681ee5eaa
vikas <raiv2004322@gmail.com>
2022-11-05 13:14:14 +0530

    Initialized the app

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../App.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h2 className="header__title">User Card Grid Layout </h2>
+      <button className="btn">
+          Fetch Users
+      </button>
+    </header>
+  );
+};
+
+export default Header;
