4bf4c66aee8abc67924757bed1b34d50c39399d4
vikas <raiv2004322@gmail.com>
2022-11-10 11:28:36 +0530

    Added a Loader Indicator

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Loading from "./components/Loading";
 import UserCard from "./components/UserCard";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [pageNum, setPageNum] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const changePageNumber = () => {
+    setPageNum(!pageNum);
+  };
   
 
   const handleGetusers = async () => {
     try {
-      
-      const res = await fetch(`https://reqres.in/api/users?page=1`);
+      setLoading(true);
+      const res = await fetch(`https://reqres.in/api/users?page=${pageNum ? 2 : 1}`);
       const users = await res.json();
       setUsers(users.data);
+      changePageNumber();
+      setLoading(false);
       
     } catch (error) {
       console.log(error);
     }
   };
 
+console.log(pageNum)
 
   return (
     <div className="app">
       <Header handleGetusers={handleGetusers} />
       <div className="container">
-        {users.map((user) => (
-          <UserCard
-            key={user.id}
-            firstName={user.first_name}
-            lastName={user.last_name}
-            email={user.email}
-            imageURL={user.avatar}
-          />
-        ))}
+        {loading ? <Loading /> : (
+          <>
+            {users.map((user) => (
+            <UserCard
+              key={user.id}
+              firstName={user.first_name}
+              lastName={user.last_name}
+              email={user.email}
+              imageURL={user.avatar}
+            />
+          ))}
+          </>
+        )}
       </div>
     </div>
   );
